d3a30c803bc931729cb2d40b0c41a4d372878142
Carlos <anqybb20@hotmail.com>
2024-04-22 16:35:47 -0600

    Primer commit, se pone la clase de Functions y la clase main

---
+++ src/test/java/mx/com/ts4/automation/Runner/Main.java
@@ -0,0 +1,7 @@
+package mx.com.ts4.automation.Runner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("HellowWorld");
+    }
+}
---
+++ src/test/java/mx/com/ts4/automation/functions/Functions.java
@@ -0,0 +1,4 @@
+package mx.com.ts4.automation.functions;
+
+public class Functions {
+}
